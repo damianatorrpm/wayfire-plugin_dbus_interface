fbe5199aa05f3c855e852413afc34ef75ea66cad
Scott Moreau <oreaus@gmail.com>
2020-11-22 15:46:16 -0700

    Only compile schemas if DESTDIR isn't set

--- compile-schemas.sh
+++ compile-schemas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+# Only compile schemas if DESTDIR isn't set
+[ ! -z "$DESTDIR" ] && exit 0
 
-glib-compile-schemas "$MESON_INSTALL_DESTDIR_PREFIX/$1"
-
-exit 0
+exec glib-compile-schemas "$MESON_INSTALL_DESTDIR_PREFIX/$1"
